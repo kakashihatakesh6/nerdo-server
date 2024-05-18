ae6027b8841909fe9b0b881225232ac2617048b7
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-05-18 11:23:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://sages:sages@sages.p1zye6m.mongodb.net/habit-app").then(() => {
+    console.log("Connected to MongoDB!");
+}).catch((error) => {
+    console.log("Error Connecting to MOnoOG", error);
+});
+
+app.listen(port, () => {
+    console.log("Server running on port 3000");
+});
+
+
+
+const Habit = require('./models/habit');
+// Endpoints to create a habit in a backend
+app.post("/habits", async (req, res) => {
+    try {
+        const {title, color, repeatMode, reminder} = req.body;
+        const newHabit = new Habit({
+            title,
+            color,
+            repeatMode,
+            reminder
+        })
+        console.log(newHabit)
+
+        const savedHabit = await newHabit.save();
+        res.status(200).json(savedHabit);
+
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"});
+    }
+});
+
+
+app.get("/habitslist", async (req, res) => {
+    try {
+        const allHabits = await Habit.find({});
+
+        res.status(200).json(allHabits);
+        
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+app.put("/habits/:habitId/completed", async (req, res) => {
+    const habitId = req.params.habitId;
+    const updatedCompletion = req.body.completed; // The updated completion object
+    
+    try {
+        const updatedHabit = await Habit.findByIdAndUpdate(
+            habitId,
+            { completed: updatedCompletion },
+            { new: true }
+        );
+        console.log("IDDD =>", habitId, updatedHabit)
+
+        if (!updatedHabit) {
+            return res.status(404).json({error: "Habit not found"})
+        }
+
+        res.status(200).json(updatedHabit);
+
+    } catch (error) {
+        console.log("Error", error);
+        res.status(500).json({error: error.message})
+    }
+});
+
+app.delete("/habits/:habitId", async (req, res) => {
+    try {
+        const {habitId} = req.params;
+
+        await Habit.findByIdAndDelete(habitId);
+        res.status(200).json({message: "Habit deleted successfully!"})
+    } catch (error) {
+        res.status(500).json({error: "Unable to delete the habit"})
+    }
+})
